16a1ddf444581520b7028f29524547d52ce0a56c
Mark123M <mark123ymm@gmail.com>
2022-08-03 16:22:56 -0400

    password hash

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,8 +4,19 @@ const bcrypt = require('bcrypt')
 
 //member registration
 router.post("/register",async (req, res)=>{
-    const newUser = new User(req.body)
     try{
+        //generate new password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        console.log(hashedPassword)
+
+        //create new user
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+
         //save user
         const user = await newUser.save() //waits until the process is finished before executing next line
         res.status(200).json(user)
@@ -19,11 +30,12 @@ router.post("/login", async(req, res)=>{
     try{
         //find user with matching email
         const user = await User.findOne({email: req.body.email});
+        const isValid = await bcrypt.compare(req.body.password, user.password)
 
         if(!user){
             res.status(404).json('user not found')
         }
-        else if(req.body.password != user.password){
+        else if(isValid === false){
             res.status(400).json('incorrect password')
         } 
         else{
