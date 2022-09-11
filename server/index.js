418df21052c7eefcef2b2b3e04cca9470a449f7b
Mark123M <84888860+Mark123M@users.noreply.github.com>
2022-09-11 08:12:58 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -42,6 +42,7 @@ app.get('/users',(req, res)=>{
   })
 }) */
 
-app.listen(3001, ()=>{
-    console.log('hello world')
+const PORT = process.env.PORT || 3001
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
 })
